d6bc8599ae02e18106673d5a9547736f2a89748e
Alvaro <alvarogarnica1997@gmail.com>
2022-11-12 02:38:55 +0100

    pep8 + rename + new function

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,29 @@
+import pandas as pd
+import os
+import numpy as np
+
+
 def number_of_matching_characters(str1, str2):
     str1 = str1.replace(" ", "")
     str2 = str2.replace(" ", "")
-    c = 0   
-    
+    c = 0
+
     for i in range(len(str1)):
         if str1[i] == str2[i]:
             c += 1
-    return c 
+    return c
 
-def top_signos(df, variable_rank, *args):
+
+def top_signos(df, variable_rank, estimados, *args):
     df_ = pd.DataFrame()
-    jugadas = df.sort_values(variable_rank).Jugada.str.replace(' ','')
-    
+    jugadas = df.sort_values(variable_rank).Jugada.str.replace(' ', '')
+
     for c in args:
         unos = []
         equis = []
         doses = []
         for partido in range(1, 15):
-            _ = jugadas[:c].str[(partido-1):partido].value_counts()
+            _ = jugadas[:c].str[(partido - 1):partido].value_counts()
             try:
                 unos.append(_['1'])
             except:
@@ -31,68 +37,70 @@ def top_signos(df, variable_rank, *args):
             except:
                 doses.append(0)
         df_ = pd.concat([df_, pd.DataFrame({'1': unos, 'X': equis, '2': doses}, index=estimados.index)], axis=1)
-    df_.columns=pd.MultiIndex.from_product([[f'Top  {arg}' for arg in (args)], ['1', 'X', '2']])
+    df_.columns = pd.MultiIndex.from_product([[f'Top  {arg}' for arg in args], ['1', 'X', '2']])
     return df_
 
-def jugadas_con_premio(jugada, n):   
+
+def jugadas_con_premio(jugada, n):
     jugada = jugada.replace(' ', '')
-    
-    if n==12:     
+
+    if n == 12:
         l = []
         for idx, s in enumerate(jugada):
             _ = jugada[idx + 1:]
-            if s=='1':
+            if s == '1':
                 opc1 = jugada[:idx] + 'X'
                 opc2 = jugada[:idx] + '2'
-            elif s=='X':
+            elif s == 'X':
                 opc1 = jugada[:idx] + '1'
                 opc2 = jugada[:idx] + '2'
-            elif s=='2':
+            elif s == '2':
                 opc1 = jugada[:idx] + '1'
                 opc2 = jugada[:idx] + 'X'
 
             for idx2, s2 in enumerate(_):
-                if s2=='1':
+                if s2 == '1':
                     l.append(" ".join(opc1 + _[:idx2] + 'X' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + 'X' + _[idx2 + 1:]))
                     l.append(" ".join(opc1 + _[:idx2] + '2' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + '2' + _[idx2 + 1:]))
-                elif s2=='X':
+                elif s2 == 'X':
                     l.append(" ".join(opc1 + _[:idx2] + '1' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + '1' + _[idx2 + 1:]))
                     l.append(" ".join(opc1 + _[:idx2] + '2' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + '2' + _[idx2 + 1:]))
-                elif s2=='2':
+                elif s2 == '2':
                     l.append(" ".join(opc1 + _[:idx2] + '1' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + '1' + _[idx2 + 1:]))
                     l.append(" ".join(opc1 + _[:idx2] + 'X' + _[idx2 + 1:]))
                     l.append(" ".join(opc2 + _[:idx2] + 'X' + _[idx2 + 1:]))
-    elif n==13:
-        l=[]
+    elif n == 13:
+        l = []
         for idx, s in enumerate(jugada):
-            if s=='1':
+            if s == '1':
                 j = jugada[:idx] + 'X' + jugada[idx + 1:]
                 l.append(" ".join(j))
                 j = jugada[:idx] + '2' + jugada[idx + 1:]
                 l.append(" ".join(j))
-            elif s=='X':
+            elif s == 'X':
                 j = jugada[:idx] + '1' + jugada[idx + 1:]
                 l.append(" ".join(j))
                 j = jugada[:idx] + '2' + jugada[idx + 1:]
                 l.append(" ".join(j))
-            elif s=='2':
+            elif s == '2':
                 j = jugada[:idx] + '1' + jugada[idx + 1:]
                 l.append(" ".join(j))
                 j = jugada[:idx] + 'X' + jugada[idx + 1:]
                 l.append(" ".join(j))
-    return l 
+    return l
 
-def tabla_jugadas(mis_jugadas):  
+
+def tabla_jugadas(mis_jugadas, estimados):
     """
     función que crea dataframe del porcentaje de jugados
     """
     mis_jugadas = [jugada.replace(' ', '') for jugada in mis_jugadas]
-    
+
     _1 = []
     _X = []
     _2 = []
@@ -104,7 +112,7 @@ def tabla_jugadas(mis_jugadas):
         for jugada in mis_jugadas:
             if jugada[i] == '1':
                 c1 += 1
-            elif jugada[i] == 'X': 
+            elif jugada[i] == 'X':
                 cx += 1
             else:
                 c2 += 1
@@ -113,6 +121,41 @@ def tabla_jugadas(mis_jugadas):
         _X.append(cx)
         _2.append(c2)
 
-        
-    d = {'1':_1, 'X':_X, '2':_2}
-    return(round(pd.DataFrame(d, index = list(estimados.index.values))/len(mis_jugadas),2))
+    d = {'1': _1, 'X': _X, '2': _2}
+    return round(pd.DataFrame(d, index=list(estimados.index.values)) / len(mis_jugadas), 2)
+
+
+def jugadas_peñas(df: pd.DataFrame, path_to_jugadas: str, n_jornada: str, año: str):
+    """
+    lee las jugadas de las peñas que estén en la ruta especificada, y añade una columna al df
+
+    :param df: dataframe base de jugadas
+    :param path_to_jugadas: ruta que contiene archivos txt con las jugadas de las peñas
+    :param n_jornada: número de la jornada (e.g. 1,2,3,4,etc)
+    :param año: año de la temporada (si es la temporada 22-23 el año será 2023)
+    :return: df base con 2 nuevas columnas,
+    """
+    all_peñas = os.listdir(path_to_jugadas)
+    all_jugadas = []
+    nombres_peñas = []
+    for peña in all_peñas:
+        with open(f'{path_to_jugadas}/{peña}') as file:
+            jugadas_peña = file.readlines() 
+            jugadas_peña = [" ".join(jugada_peña.rstrip()[:14]) for jugada_peña in jugadas_peña]
+        nombres_peñas.extend([peña.split('.')[0].split(f'{n_jornada}_{año}_')[1].replace('_', ' ')]*len(jugadas_peña))
+        all_jugadas += jugadas_peña
+
+    print(f'Cruzando {len(all_jugadas)} jugadas, {len(set(all_jugadas))} de ellas únicas, de {len(all_peñas)} peñas diferentes...')
+    
+    all_jugadas = pd.DataFrame({'jugada': all_jugadas, 'peñas': nombres_peñas})\
+                    .groupby('jugada').agg(total_peñas=('peñas', 'count'), peñas=('peñas', lambda x: x.to_list()))
+    df = pd.merge(df, all_jugadas, right_index=True, left_on='Jugada', how='left')
+    df['total_peñas'] = df['total_peñas'].fillna(0).astype(int)
+    df['peñas'] = df['peñas'].fillna(np.nan)
+    
+    return df
+
+
+# TODO: funcion que coge un df cualqiera lo abre en un gui y el user puede cambiarlo manualmente (lo que hago con estimados)
+def manually_update(df: pd.DataFrame) -> pd.DataFrame:
+    pass
